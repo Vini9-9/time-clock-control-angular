107131b38ff079cab8b893ac1f025fc31b30dd4e
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-06-28 10:38:53 -0300

    refactor: navBar receive logo image as att

--- time-clock-control/src/app/app.component.ts
+++ time-clock-control/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'time-clock-control';
+  logoHeader = "../assets/click-clock-header.png"; 
 }
--- time-clock-control/src/app/nav-bar/nav-bar.component.ts
+++ time-clock-control/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css', '../app.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  @Input() logo: string | undefined;
   constructor() { }
 
   ngOnInit(): void {
