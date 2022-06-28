da90bcb10145bff5b4cda6f79d5f17126625069f
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-06-28 14:52:23 -0300

    fix: correct the file with global styles

--- time-clock-control/src/app/nav-bar/nav-bar.component.ts
+++ time-clock-control/src/app/nav-bar/nav-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css', '../app.component.css']
+  styleUrls: ['./nav-bar.component.css',]
 })
 export class NavBarComponent implements OnInit {
   @Input() logo: string | undefined;
