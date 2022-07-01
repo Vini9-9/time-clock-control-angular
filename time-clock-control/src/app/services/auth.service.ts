651a82adcc744e3dd5bc398649a4a4fbc8327132
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-07-01 15:16:15 -0300

    auth service getting token

--- time-clock-control/src/app/services/auth.service.ts
+++ time-clock-control/src/app/services/auth.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { UserService } from './users.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private userService: UserService
+    ) { }
 
   authUrl = 'http://localhost:3000/auth';
   token = '';
@@ -14,12 +18,16 @@ export class AuthService {
   public authUser(email: string, password: string): Observable<any> {
     const body = JSON.stringify({email, password});
 
-    return this.http.post(this.authUrl, {
+    return this.http.request('POST', this.authUrl, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       }),
       body: body
-    })
+    }).pipe(
+      tap((res:any) => {
+      const authToken = res.token;
+      this.userService.saveToken(authToken);
+    }))
   }
 
 }
